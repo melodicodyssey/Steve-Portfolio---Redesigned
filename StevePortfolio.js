c1e3ed1764e8dbb508477223d598f88f29d7c5b4
azureowl <rochellmae@gmail.com>
2015-06-30 15:12:56 -0700

    Edit copy, separated CSS and js in new files

---
+++ StevePortfolio.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $(".share-nav").on("click", function () {
+      showInfo($(this), ".info-share");
+    });
+    $(".aquaintly-nav").on("click", function () {
+      showInfo($(this), ".info-aquaintly");
+    });
+    $(".gnosis-nav").on("click", function () {
+      showInfo($(this), ".info-gnosis");
+    });
+    $(".bio-nav").on("click", function () {
+      showInfo($(this), ".info-bio");
+    });
+    $(".contact-nav").on("click", function () {
+      showInfo($(this), ".info-contact");
+    });
+
+    var infoArray = [$(".info-bio"), $(".info-contact"), $(".info-gnosis"), $(".info-aquaintly"), $(".info-helio"), $(".info-share")];  
+
+    function hideArray () {
+      for (var i = 0; i < infoArray.length; i++) {
+        infoArray[i].hide();
+      }
+    }
+    function showInfo (thisElement,elementInfo) {
+      hideArray();
+      thisElement.closest(".page-wrapper").find(elementInfo).fadeToggle("slow");
+    }
+  });
